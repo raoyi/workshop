1561ae8888a7d44e7d54c8956a1b3d13a97de1bd
Rao.Yi <raoyi@users.noreply.github.com>
2019-06-17 09:38:52 +0800

    Update ocr.py

--- 文字识别/ocr.py
+++ 文字识别/ocr.py
@@ -4,4 +4,7 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'D:\Program Files\Tesseract-OCR\tesseract.exe'
 img = Image.open('txt.jpg')
 
-print(pytesseract.image_to_string(img, lang = 'chi_sim'))
+text = pytesseract.image_to_string(img, lang = 'chi_sim')
+text = unicode(text, "utf-8")
+
+print(text)
