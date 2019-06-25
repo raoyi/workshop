554c3e0ec5b681b48dd0a182a3c002a883d2590a
prcraoyi <prcraoyi@qq.com>
2019-06-25 10:58:25 +0800

    format

--- 文字识别/ocr.py
+++ 文字识别/ocr.py
@@ -4,7 +4,7 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'D:\Program Files\Tesseract-OCR\tesseract.exe'
 img = Image.open('txt.jpg')
 
-text = pytesseract.image_to_string(img, lang = 'chi_sim')
+text = pytesseract.image_to_string(img, lang='chi_sim')
 text = unicode(text, "utf-8")
 
 print(text)
--- 码识别/qr_read/barcode.py
+++ 码识别/qr_read/barcode.py
@@ -4,23 +4,21 @@ import argparse
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-ap.add_argument("-f", "--file", required=False,
-        help="path to save barcode")
+ap.add_argument("-i", "--image", required=True, help="path to input image")
+ap.add_argument("-f", "--file", required=False, help="path to save barcode")
 args = vars(ap.parse_args())
 
 #get barcodes
 barcodes = decode(Image.open(args["image"]))
 num = len(barcodes)
 
-if args["file"]==None:
+if args["file"] == None:
     for i in range(num):
         print(barcodes[i][0].decode('utf-8'))
-        
+
 else:
     f = open(args["file"], 'w')
     for i in range(num):
-        f.write(barcodes[i][0].decode('utf-8')+'\n')
-        
+        f.write(barcodes[i][0].decode('utf-8') + '\n')
+
     f.close()
